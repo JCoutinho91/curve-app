ac564391221711c15288eba05a644aa56e0beafe
JosePedro <guedes.jose@gmail.com>
2022-03-24 19:41:28 +0000

    Home Screen partially done

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import styles from "./styles";
+const image = require("../../../assets/images/Crypto.png");
 
 const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>HomeScreen</Text>
+    <View style={styles.root}>
+      <Image style={styles.image} source={image} />
+
+      <Text style={styles.header1}>Start Your Investing Journey Here!</Text>
+      <Text style={styles.header2}>
+        Manage your virtual $100.00 and compete with others.
+      </Text>
     </View>
   );
 };
---
+++ src/screens/HomeScreen/styles.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  root: {
+    padding: 20,
+    alignItems: "center",
+    backgroundColor: "white",
+    height: "100%",
+  },
+
+  image: {
+    height: "70%",
+    aspectRatio: 1,
+  },
+  header1: {
+    marginTop: -100,
+    fontSize: 30,
+    color: "darkblue",
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 20,
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 50,
+  },
+  header2: {
+    paddingTop: 30,
+    fontSize: 20,
+    textAlign: "center",
+    color: "black",
+  },
+});
+
+export default styles;
